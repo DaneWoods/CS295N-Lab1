da2d9ab1dd010bc57081458006e81cb8984135a9
DaneWoods <megadster@comcast.net>
2019-10-13 17:17:09 -0700

    Added a name property to the story class, 10/13/2019

--- SkeletonSite/Models/Story.cs
+++ SkeletonSite/Models/Story.cs
@@ -8,6 +8,8 @@ namespace SkeletonSite.Models
 {
     public class Story
     {
+        [Required(ErrorMessage = "Please input the creator of the story")]
+        public string Name { get; set; }
         [Required(ErrorMessage = "Please input Some text for your story")]
         public string Text { get; set; }
     }
